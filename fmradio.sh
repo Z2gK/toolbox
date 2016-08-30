f75fb76abe0fa5f019f2ae6fa25f3b58d9ca71ef
cgh-coursera <muscadomestica@hotmail.com>
2016-08-30 14:36:53 +0000

    update fm

--- fmradio.sh
+++ fmradio.sh
@@ -9,3 +9,8 @@ FREQ=$1
 
 rtl_fm -f "$FREQ"e6 -M wbfm -s 200000 -r 48000 - | aplay -r 48000 -f S16_LE
 
+
+# See www.ibm.com/developerworks/library/l-job-terminating
+# for info on terminating processes at a certain time
+
+
