1cb2308ca7aa8fe6a18ee4f5cbe3cd22a9a3a9c6
cgh-coursera <muscadomestica@hotmail.com>
2016-09-02 15:49:52 +0800

    updated recordradio script

--- recordradio.sh
+++ recordradio.sh
@@ -23,12 +23,23 @@ fi
 DATESTRING=`date +%Y%m%d`
 OUTPUTFILENAME="$OUTPUTDIR"/rec"$DATESTRING".pcm
 
-rtl_fm -f "$FREQ"e6 -M wbfm -s 200000 -r 48000 $OUTPUTFILENAME &
-RTLPID=$!
-
+#rtl_fm -f "$FREQ"e6 -M wbfm -s 200000 -r 48000 $OUTPUTFILENAME &
+#RTLPID=$!
 # sleep 130m # sleep for 2 hours 10 mins - for a recording of that length
-sleep 10s  # sleep for 10 seconds
-kill -15 $RTLPID    # check if this signal is correct
+#sleep 20s  # sleep for 10 seconds
+#kill -15 $RTLPID    # check if this signal is correct
+
+timeout 130m rtl_fm -f "$FREQ"e6 -M wbfm -s 200000 -r 48000 $OUTPUTFILENAME
+
 echo "recording done"
 
-# aplay -r 48000 -f S16_LE
+# Command to play
+# aplay -r 48000 -f S16_LE <filename>
+
+
+# crontab installation and configuration
+# systemctl enable cronie
+# VISUAL=emacs crontab -e
+# Enter this crontab line
+# min(0-59) hour(0-23) dayofmonth(1-31) month(1-12) dayofweek(0-6,sun-sat) command
+# 55          20           *              *            5     $HOME/bin/recordradio.sh 98.0 <directory-to-save>
