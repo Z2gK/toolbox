468a702fd977ef33b08b6ef5b35876e0a639b11d
cgh-coursera <muscadomestica@hotmail.com>
2016-11-28 18:22:25 +0800

    updated script

--- sym924nowplaying.py
+++ sym924nowplaying.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+URL="http://radio.toggle.sg/static/symphony/billboard.xml"
+
 tree = ET.parse('nowplay.xml')
 tree.getroot()
 
@@ -6,7 +8,31 @@ rt = tree.getroot()
 print (rt.tag)
 
 for child in rt:
-    for child2 in child:
-        print(child2.tag)
+    # print child.tag, child.attrib
+    x = child.attrib.get('eventType')
+    print x
+    if (x == 'Song'):
+        print "Song detected"
+        for child2 in child:
+            title = child2.attrib.get('title')
+            print title
+            for child3 in child2:
+                if (child3.tag == '{urn:schemas-rcsworks-com:SongSchema}Artist'):
+                    player = child3.attrib.get('name')
+                if (child3.tag == '{urn:schemas-rcsworks-com:SongSchema}Album'):
+                    album = child3.attrib.get('title')
+            print player, album
+            # Fill in list of triplet title, player, album    
 
+
+# print list and format in gui
+
+
+#for child in rt.findall('Event'):
+#    x = child.attrib.get('eventID')
+#    y = child.attrib.get('eventType')
+#    print x,y
+
+
+        
     
