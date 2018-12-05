71f66f7593cc672d8f39cc8591c427b58007d5a0
Z2gK <rubus@dell.localdomain>
2018-12-05 18:49:53 +0800

    edited findduplicates

--- findduplicates.py
+++ findduplicates.py
@@ -12,7 +12,8 @@
 import os, hashlib
 import sys
 
-def file_hash(filename):
+# This function may have a problem with zero byte files
+def filehash(filename):
     ''' Returns the SHA256 hash of a file.
     The return value is a string'''
     h = hashlib.sha256()
@@ -22,14 +23,61 @@ def file_hash(filename):
             return h.hexdigest()
 
 
+def readpathlist(fp):
+    ''' Takes a file pointer as the input and goes through the paths on each line
+    Returns a list of paths provided in this file
+    WARNING: No error checking is implemented! '''
+    return [x.strip() for x in fp]
+        
+
+
+def duplicates(filelist):
+    ''' This function takes a list of filepaths as input and returns 
+    a dictionary of information about the duplicates. 
+    <key, value> = <hash of file, (list of filepaths where this occurs)>'''
+    allfiles = {} # Stores a dict of all files scanned so far
+    dups = {} # Stores a dict of duplicates found so far
+    for path in filelist:
+        for root, dirs, files in os.walk(path):
+            print("Going through " + root)
+            for name in files:
+                filepath = os.path.join(root,name)
+                h = filehash(filepath) # may have a problem with zero byte files
+                print(filepath)
+                print(h)
+                if h in allfiles:      # this file has been seen before
+                    if h in dups:  # this file has been seen at least twice
+                        dups[h].append(filepath)
+                    else:  # this file has been seen only once
+                        dups[h] = [allfiles[h], filepath]
+                else: # this file has not been seen before
+                    allfiles[h] = filepath
+    return dups
+
+def unique():
+    ''' For unique files '''
+
+def pickledict():
+    ''' This function takes in a dictionary and a filename and saves the dictionary to the filename'''
+
+def printresult(d):
+    ''' This function takes a dictionary as the input and prints it to standard output '''
+    for k, v in d:
+        for s in v:
+            print(s)
+        print("\n")
 
 filename = sys.argv[1]
-thehash = file_hash(filename)
-print(thehash)
-print(type(thehash))
+#thehash = file_hash(filename)
+#print(thehash)
+#print(type(thehash))
 
+with open(filename, "r") as fp:
+    filelist = readpathlist(fp)
 
-
+print(filelist)
+d = duplicates(filelist)
+print(d)
 
 # print(file_hash(filename))
 
